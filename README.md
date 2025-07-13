137c1aadca7f40f594f5de43d2267daddbce5a1f
JangHyeonJun <totok682@naver.com>
2025-07-13 19:50:07 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# cpp-socket-server
+
+🎯 **Boost.Asio 기반 C++20 Echo Server**
+
+이 프로젝트는 `Boost.Asio`와 C++20의 `co_await` 코루틴을 사용하여 만든 간단한 TCP 에코 서버입니다. 실습 목적이며, 이후 확장 가능한 구조로 발전시킬 계획입니다.
+
+---
+
+## 🔧 요구사항
+
+- Windows 11 64bit
+- Visual Studio 2022
+- C++20 지원
+- Boost (vcpkg 사용 권장)
+
+```bash
+# vcpkg 설치 후
+vcpkg install boost-asio
---
+++ main.cpp
@@ -0,0 +1,85 @@
+#include <boost/asio.hpp>
+#include <iostream>
+#include <format>
+#include <string>
+
+using boost::asio::ip::tcp;
+
+#if defined(BOOST_ASIO_ENABLE_HANDLER_TRACKING)
+# define use_awaitable \
+  boost::asio::use_awaitable_t(__FILE__, __LINE__, __PRETTY_FUNCTION__)
+#endif
+
+boost::asio::awaitable<void> echo(tcp::socket socket)
+{
+	try
+	{
+		char data[1024];
+		for (;;)
+		{
+			std::size_t n = co_await socket.async_read_some(boost::asio::buffer(data), boost::asio::use_awaitable);
+			std::string response(data, n);
+			response = "\r\n response: " + response + "\r\n";
+			
+			co_await async_write(socket, boost::asio::buffer(response), boost::asio::use_awaitable);
+		}
+	}
+	catch (std::exception& e)
+	{
+		std::cout << std::format("echo Exception: {}\n", e.what());
+	}
+}
+
+boost::asio::awaitable<void> listener(boost::asio::ip::port_type port)
+{
+	auto executor = co_await boost::asio::this_coro::executor;
+	tcp::acceptor acceptor(executor, { tcp::v4(), port });
+
+	for (;;)
+	{
+		tcp::socket socket = co_await acceptor.async_accept(boost::asio::use_awaitable);
+		boost::asio::co_spawn(executor, echo(std::move(socket)), boost::asio::detached);
+	}
+}
+
+uint16_t input_port(uint16_t default_port) {
+	std::cout << "포트번호를 입력해주세요 (default: " << default_port << "): ";
+	std::string input;
+	std::getline(std::cin, input);
+
+	if (input.empty()) return default_port;
+
+	int value = default_port; // 임시로 int 사용 (from_chars는 부호 있는 정수 파싱)
+	auto [ptr, ec] = std::from_chars(input.data(), input.data() + input.size(), value);
+
+	if (ec == std::errc() && value >= 0 && value <= 65535) {
+		return static_cast<uint16_t>(value);
+	}
+	else {
+		std::cout << "잘못된 입력입니다. 기본값(" << default_port << ") 사용\n";
+		return default_port;
+	}
+}
+
+
+int main()
+{
+	constexpr uint16_t default_port = 12345;
+	auto port = input_port(default_port);
+	std::cout << "선택된 포트: " << port << std::endl;
+
+	try
+	{
+		boost::asio::io_context io_context(1);
+		
+		boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
+		signals.async_wait([&](auto, auto) { io_context.stop(); });
+
+		boost::asio::co_spawn(io_context, listener(port), boost::asio::detached);
+		io_context.run();
+	}
+	catch (std::exception& e)
+	{
+		std::cout << std::format("Exception: {}\n", e.what());
+	}
+}
