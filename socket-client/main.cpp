fd04e3ffec3fabd14aa78585f16a2952f87e7f16
JangHyeonJun <totok682@naver.com>
2025-07-20 20:37:15 +0900

    feat: 공용 라이브러리 프로젝트 추가, tcp 타임서버/클라이언트 구현(2)

--- socket-client/main.cpp
+++ socket-client/main.cpp
@@ -4,37 +4,77 @@
 #include <iostream>
 using boost::asio::ip::tcp;
 
+
+std::string get_host(int argc, char* argv[]) {
+	if (argc >= 2 && argv[1] && argv[1][0] != '\0') {
+		return argv[1];
+	}
+	std::cout << "서버 호스트를 입력하세요 (default: " << share::DefaultHost << "): ";
+	std::string host;
+	std::getline(std::cin, host);
+	if (host.empty())
+		return share::DefaultHost;
+	return host;
+}
+
+unsigned short get_port(int argc, char* argv[]) {
+	if (argc >= 3 && argv[2] && argv[2][0] != '\0') {
+		try {
+			return static_cast<unsigned short>(std::stoi(argv[2]));
+		}
+		catch (...) {}
+	}
+	std::cout << "서버 포트번호를 입력하세요 (default: " << share::DefaultPort << "): ";
+	std::string port_str;
+	std::getline(std::cin, port_str);
+	if (port_str.empty())
+		return share::DefaultPort;
+	try {
+		return static_cast<unsigned short>(std::stoi(port_str));
+	}
+	catch (...) {
+		std::cout << "입력이 올바르지 않아 기본값(" << share::DefaultPort << ")을 사용합니다.\n";
+		return share::DefaultPort;
+	}
+}
+
+void get_time_now(int argc, char* argv[])
+{
+	auto host = get_host(argc, argv);
+	auto port = get_port(argc, argv);
+
+	boost::asio::io_context io_context;
+	tcp::resolver resolver(io_context);
+
+	tcp::resolver::results_type endpoints = resolver.resolve(host, std::to_string(port));
+	tcp::socket socket(io_context);
+	boost::asio::connect(socket, endpoints);
+
+	for (;;)
+	{
+		std::array<char, 128> buf;
+		boost::system::error_code error;
+
+		size_t len = socket.read_some(boost::asio::buffer(buf), error);
+		if (error == boost::asio::error::eof)
+			break; // Connect close by peer
+		else if (error)
+			throw boost::system::system_error(error); // Error
+
+		std::cout.write(buf.data(), len);
+		std::cout << std::endl;
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	try
 	{
-		if (argc != 2)
-		{
-			std::cerr << "client: <host>" << std::endl;
-			return 1;
-		}
-
-		boost::asio::io_context io_context;
-		tcp::resolver resolver(io_context);
-
-		tcp::resolver::results_type endpoints = resolver.resolve(argv[1], "daytime");
-		tcp::socket socket(io_context);
-		boost::asio::connect(socket, endpoints);
-
 		for (;;)
-		{
-			std::array<char, 128> buf;
-			boost::system::error_code error;
+			get_time_now(argc, argv);
 
-			size_t len = socket.read_some(boost::asio::buffer(buf), error);
-			if (error == boost::asio::error::eof)
-				break; // Connect close by peer
-			else if (error)
-				throw boost::system::system_error(error); // Error
-
-			std::cout.write(buf.data(), len);
-		}
-
+		std::cout << "\n아무 키나 누르면 종료합니다...";
+		std::cin.get();
 		return 0;
 	}
 	catch (std::exception& e)
--- socket-server/main.cpp
+++ socket-server/main.cpp
@@ -1,10 +1,42 @@
-#include 
+#include "share.h"
 #include <ctime>
 #include <iostream>
 #include <string>
 #include <boost/asio.hpp>
 
 using boost::asio::ip::tcp;
+
+unsigned short get_port(int argc, char* argv[]) 
+{
+    unsigned short port = share::DefaultPort;
+
+    // 명령행 인자가 있을 때만 파싱 시도
+    if (argc >= 2 && argv[1] && argv[1][0] != '\0') {
+        try {
+            port = static_cast<unsigned short>(std::stoi(argv[1]));
+            return port;
+        }
+        catch (...) {
+            std::cout << "명령행 인자 포트값이 잘못되어 무시합니다.\n";
+        }
+    }
+
+    // 명령행 인자가 없거나 파싱 실패 시 입력 받기
+    std::cout << "서버 포트번호를 입력하세요 (default: " << share::DefaultPort << "): ";
+    std::string port_str;
+    std::getline(std::cin, port_str);
+    if (port_str.empty())
+        return share::DefaultPort;
+    try {
+        port = static_cast<unsigned short>(std::stoi(port_str));
+    }
+    catch (...) {
+        std::cout << "입력이 올바르지 않아 기본값(" << share::DefaultPort << ")을 사용합니다.\n";
+        port = share::DefaultPort;
+    }
+
+    return port;
+}
 
 std::string make_daytime_string()
 {
@@ -17,14 +49,27 @@ int main(int argc, char* argv[])
 {
 	try
 	{
+		auto port = get_port(argc, argv);
 		boost::asio::io_context io_context;
-		tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 1234)
+		tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), port));
 
+		for (;;)
+		{
+			tcp::socket socket(io_context);
+			acceptor.accept(socket);
+
+			std::string message = make_daytime_string();
+
+			boost::system::error_code ignored_error;
+			boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+		}
 	}
 	catch (std::exception& e)
 	{
 		std::cout << std::format("Exception: {}\n", e.what());
 	}
 
+	std::cout << "\n아무 키나 누르면 종료합니다...";
+	std::cin.get();
 	return 0;
 }
